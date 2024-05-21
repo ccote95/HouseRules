332aea001772147854e60336be1d3d96681a5662
Chad Cote <chadcote58@gmail.com>
2024-05-21 10:57:13 -0400

    Created a function to get a user by its id

--- client/src/components/UserProfileDetails.jsx
+++ client/src/components/UserProfileDetails.jsx
@@ -1,5 +1,12 @@
+import { useEffect, useState } from "react"
+
 export default function UserDetails()
 {
+    const [user,setUser]=useState()
+
+    useEffect(()=> {
+        
+    },[])
     return(
         <div>hello</div>
     )
--- client/src/managers/userProfileManager.js
+++ client/src/managers/userProfileManager.js
@@ -4,3 +4,7 @@ const _apiUrl = "/api/userprofile";
 export const getUsers = () => {
     return fetch(_apiUrl).then((res) => res.json());
 }
+
+export const getUserById = (id) => {
+    return fetch(`_apiUrl/{id}`).then((res) => res.json());
+}
