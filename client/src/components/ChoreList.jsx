351aae54e6c8b2187abd5f425bd87bc6daf68767
Chad Cote <chadcote58@gmail.com>
2024-05-21 15:35:19 -0400

    Rendered the chore list with delete buttons

--- client/src/components/ChoreList.jsx
+++ client/src/components/ChoreList.jsx
@@ -1,6 +1,36 @@
+import { useEffect, useState } from "react"
+import { getChores } from "../managers/choreManager.js"
+import { Button, Card, CardBody, CardText, CardTitle } from "reactstrap"
+
 export default function ChoreList()
 {
+    const[chores, setChores]=useState([])
+
+    useEffect(() => {
+        getChores().then(setChores)
+    },[])
     return(
-        <div>hello</div>
+        <div className="grid gap-0 row-gap-3" style={{display: 'flex', flexDirection: 'column'}}>
+            {chores.map((chore) => {
+                return(
+                    <Card className="p-1">
+                        <CardBody>
+                           <CardTitle tag={"h5"}>
+                            {chore.name}
+                            </CardTitle>
+                                <CardText>
+                                Chore Difficulty: {chore.difficulty}
+                                </CardText> 
+                                <CardText>
+                                    Chore Frequency: {chore.choreFrequencyDays}
+                                    
+                                    <Button style={{float: "right"}}>DELETE</Button>
+                                </CardText>
+                               
+                            </CardBody>
+                    </Card>
+                )
+            })}
+        </div>
     )
 }
