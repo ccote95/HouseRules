c30451eb281a87ac0faf69b73616d8851b5ba004
Chad Cote <chadcote58@gmail.com>
2024-05-21 09:59:32 -0400

    Rendered the userprofiles with their name,email,address

--- client/src/components/UserProfileList.jsx
+++ client/src/components/UserProfileList.jsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from "react"
+import { getUsers } from "../managers/userProfileManager.js"
+
 export default function UserProfileList()
 {
+    const [users, setUsers] = useState()
+    useEffect(() => {
+        getUsers().then(setUsers)
+    },[])
     return (
-        <div>hello</div>
+        <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px', justifyContent: 'center' }}>
+            {users?.map((user) => {
+                return(
+                    <div >
+                        <div className="card" style={{width: '18rem', margin:'auto', marginTop:'10px'}}>
+                            <ul className="list-group list-group-flush">
+                                <li className="list-group-item">{user.firstName} {user.lastName}</li>
+                                <li className="list-group-item">{user.email}</li>
+                                <li className="list-group-item">{user.address}</li>
+                            </ul>
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
     )
 }
